80af1f7cfa4fc910541c5e47c7c34c453f83911d
MatCodes <contact@mat.codes>
2026-03-27 08:52:54 +1100

    Debugging DB backups

--- debian/forge-craftcms-s3-backups.sh
+++ debian/forge-craftcms-s3-backups.sh
@@ -124,19 +124,24 @@ s3_sync_files() {
 
 # Sync DB backup directory
 s3_sync_db_backups() {
+    DB_S3_BACKUP_TARGET="${S3_BACKUP_TARGET}/_DB-Backups"
 
-    printf "Backup dir: ${DB_BACKUP_DIRECTORY}\n"
-    printf "Backup tar: ${S3_BACKUP_TARGET}\n"
+    printf "[DEBUG][DBSYNC] Backup dir: %s\n" "${DB_BACKUP_DIRECTORY}"
+    printf "[DEBUG][DBSYNC] Backup target: %s\n" "${DB_S3_BACKUP_TARGET}"
 
-    aws s3 sync "${DB_BACKUP_DIRECTORY}" "${S3_BACKUP_TARGET}" \
-        --profile craftcms-backups
-    # if aws s3 sync "${DB_BACKUP_DIRECTORY}" "${S3_BACKUP_TARGET}" \
-    #       --profile craftcms-backups \
-    #       --no-progress \
-    #       --only-show-errors; then
-    #     printf "[SUCCESS][DBSYNC] $(date '+%Y-%m-%d %H:%M:%S') S3 sync completed for DB Backups\n"
+    # aws s3 sync "${DB_BACKUP_DIRECTORY}" "${DB_S3_BACKUP_TARGET}" \
+    #     --profile craftcms-backups \
+    #     --no-progress \
+    #     --only-show-errors
+    # STATUS=$?
+
+    # if [ "${STATUS}" -eq 0 ]; then
+    #     printf "[SUCCESS][DBSYNC] %s S3 sync completed for DB Backups\n" \
+    #         "$(date '+%Y-%m-%d %H:%M:%S')"
     # else
-    #     printf "[ERROR][DBSYNC] $(date '+%Y-%m-%d %H:%M:%S') S3 sync failed for DB Backups\n"
+    #     printf "[ERROR][DBSYNC] %s S3 sync failed for DB Backups (exit code: %s)\n" \
+    #         "$(date '+%Y-%m-%d %H:%M:%S')" "${STATUS}"
+    #     return "${STATUS}"
     # fi
 }
 
